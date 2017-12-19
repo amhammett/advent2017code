67d244054f71dfda9a8b1597ed77c6ec084712a9
Andrew Hammett <hammett@amh.io>
2017-12-18 20:12:24 -0800

    day5 part 2

---
+++ day05/src/part2.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const input_file = process.env.INPUT_FILE || 'data.txt'
+
+module.exports = {}
+
+let part1 = data => {
+  let step
+  let count = 0
+  let index = 0
+
+  while(index < data.length -1) {
+    count++
+    step = data[index]
+    if (data[index] > 2) {
+      data[index]--
+    } else {
+      data[index]++
+    }
+    index = index + parseInt(step)
+  }
+  return count
+}
+
+if (!module.parent) {
+  var fs = require('fs')
+  fs.readFile(input_file, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(part1(data.split('\n')))
+  });
+} else {
+  console.log("we were require-d from somewhere else")
+}
